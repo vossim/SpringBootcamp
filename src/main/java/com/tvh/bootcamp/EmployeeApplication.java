0f9b2acd9948ba7a970079181bf3bc253b8d7e66
Simon Vos <simon.vos@tvh.com>
2020-02-19 21:49:07 +0100

    Spring bootcamp - Boot - Configuration2

--- src/main/java/com/tvh/bootcamp/EmployeeApplication.java
+++ src/main/java/com/tvh/bootcamp/EmployeeApplication.java
@@ -1,6 +1,5 @@
 package com.tvh.bootcamp;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,17 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeApplication implements CommandLineRunner {
 
-    @Value("${employee-application.welcome-message}")
-    private String welcomeMessage;
+    private final MessageConfiguration messageConfiguration;
 
     private final EmployeeService employeeService;
 
-    public EmployeeApplication(EmployeeService employeeService) {
+    public EmployeeApplication(EmployeeService employeeService, MessageConfiguration messageConfiguration) {
         this.employeeService = employeeService;
+        this.messageConfiguration = messageConfiguration;
     }
 
     public void run(String... args) {
-        System.out.println(welcomeMessage);
+        System.out.println(messageConfiguration.getWelcomeMessage());
+        System.out.println(messageConfiguration.getDescription());
         System.out.println("-------------------------------------------");
         System.out.println("Listing all developers:");
         employeeService.printEmployeesWithRole(Role.DEVELOPER);
